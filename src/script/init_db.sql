5262de2b02d152fe327ab98dcd2598a36dfc87d7
Hakkarainen <thyyppa@gmail.com>
2017-06-12 22:48:27 +0300

    Commit 2017-06-12 TimoH_v4: init_db.sql palautettu alkuperäiseen asuun (aleksanderin)

--- src/script/init_db.sql
+++ src/script/init_db.sql
@@ -6,8 +6,29 @@ CREATE TABLE Kayttaja
     admin BOOLEAN DEFAULT 0
 );
 
-CREATE TABLE Keskustelualue(id INTEGER PRIMARY KEY, nimi VARCHAR(100) NOT NULL UNIQUE);
+CREATE TABLE Keskustelualue
+(
+  id INTEGER PRIMARY KEY,
+  nimi VARCHAR(100) NOT NULL UNIQUE
+);
 
-CREATE TABLE Keskustelu(id INTEGER PRIMARY KEY, aloittaja INTEGER NOT NULL, alue INTEGER NOT NULL, otsikko VARCHAR(100) NOT NULL, FOREIGN KEY(aloittaja) REFERENCES Kayttaja(id), FOREIGN KEY(alue) REFERENCES Keskustelualue(id));
+CREATE TABLE Keskustelu
+(
+  id INTEGER PRIMARY KEY,
+  aloittaja INTEGER NOT NULL,
+  alue INTEGER NOT NULL,
+  otsikko VARCHAR(100) NOT NULL,
+  FOREIGN KEY(aloittaja) REFERENCES Kayttaja(id),
+  FOREIGN KEY(alue) REFERENCES Keskustelualue(id)
+);
 
-CREATE TABLE Viesti(id INTEGER PRIMARY KEY, kirjoittaja INTEGER NOT NULL, keskustelu INTEGER NOT NULL, lahetysaika TIMESTAMP NOT NULL, sisalto VARCHAR(500) NOT NULL, FOREIGN KEY(kirjoittaja) REFERENCES Kayttaja(id), FOREIGN KEY(keskustelu) REFERENCES Keskustelu(id));
+CREATE TABLE Viesti
+(
+  id INTEGER PRIMARY KEY,
+  kirjoittaja INTEGER NOT NULL,
+  keskustelu INTEGER NOT NULL,
+  lahetysaika TIMESTAMP NOT NULL,
+  sisalto VARCHAR(500) NOT NULL,
+  FOREIGN KEY(kirjoittaja) REFERENCES Kayttaja(id), 
+  FOREIGN KEY(keskustelu) REFERENCES Keskustelu(id)
+);
